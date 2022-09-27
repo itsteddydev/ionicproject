2aaab0d72f6c2ea087994f58d891ace2e3d9498e
justcoddev <edisonpaulcrz@gmail.com>
2022-09-27 12:58:04 -0500

    this.places = this.placeService.getPlaces();

--- src/app/places/places.page.ts
+++ src/app/places/places.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PlacesService } from './places.service';
 
 @Component({
   selector: 'app-places',
@@ -6,28 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./places.page.scss'],
 })
 export class PlacesPage implements OnInit {
+  places=[];
 
-  public places = [
-    {
-      id: '1',
-      title: 'Eiffel Tower',
-      // eslint-disable-next-line max-len
-      imageURL: 'https://ak.uecdn.es/p/108/thumbnail/entry_id/0_isonewdb/width/660/cache_st/1657033185/type/2/bgcolor/000000/0_isonewdb.jpg',
-      comments: ['Awesome palce', 'wonderfull experience']
-    },
-    {
-      id: '1',
-      title: 'Statue of Liberty',
-      // eslint-disable-next-line max-len
-      imageURL: 'https://estatuadelalibertad.info/wp-content/uploads/2019/04/foto-estatua-de-la-libertad.jpg',
-      comments: ['Awesome palce', 'wonderfull experience']
-    },
-  ];
-
-
-  constructor() { }
+  constructor(private placeService: PlacesService) { }
 
   ngOnInit() {
+    this.places = this.placeService.getPlaces();
   }
 
 }
---
+++ src/app/places/places.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Place } from './place.model';
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+
+  private places = [
+    {
+      id: '1',
+      title: 'Eiffel Tower',
+      // eslint-disable-next-line max-len
+      imageURL: 'https://ak.uecdn.es/p/108/thumbnail/entry_id/0_isonewdb/width/660/cache_st/1657033185/type/2/bgcolor/000000/0_isonewdb.jpg',
+      comments: ['Awesome palce', 'wonderfull experience']
+    },
+    {
+      id: '1',
+      title: 'Statue of Liberty',
+      // eslint-disable-next-line max-len
+      imageURL: 'https://estatuadelalibertad.info/wp-content/uploads/2019/04/foto-estatua-de-la-libertad.jpg',
+      comments: ['Awesome palce', 'wonderfull experience']
+    },
+  ];
+
+  constructor() { }
+
+  // Obtener todos los lugares
+  getPlaces() {
+    return [...this.places];
+  }
+
+  //Obtener un solo lugar
+  getPlace(placeId: string) {
+    return {
+      ...this.places.find(place => place.id === placeId)
+    };
+  }
+
+  //Agregar un lugar
+  addPlace(title: string, imageURL: string) {
+    this.places.push({
+      title,
+      imageURL,
+      comments: [],
+      id: this.places.length + 1 + ''
+    });
+
+  }
+  //Eliminar un lugar
+  deletePlace(placeId: string) {
+    this.places = this.places.filter(place => placeId !== placeId);
+  }
+
+}
